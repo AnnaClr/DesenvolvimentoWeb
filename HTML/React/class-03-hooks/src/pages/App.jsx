5c63c08d7e3af4f7adb95ec40162277865c2c135
Ana Clara <Annaclarag2005@gmail.com>
2023-01-06 16:07:43 -0300

    add update class 03

--- HTML/React/class-03-hooks/src/pages/App.jsx
+++ HTML/React/class-03-hooks/src/pages/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 export function App() {
@@ -6,10 +6,18 @@ export function App() {
   const [count, setCount] = useState(0)
   const [text, setText] = useState('')
   const [text2, setText2] = useState('')
+  const [data, setData] = useState('')
   // console.log(name);
+  useEffect(() => {
+    if (text2) {
+      fetch(`https://api.github.com/users/${text2}`).then((response) => response.json()).then((data) => setData(data))
+    }
+  },[text2])
+
   return (
     <div className='container'>
-      {/* <h1>Class Hooks</h1>
+      { <h1>Class Hooks</h1>
+      /*
       <p>Nome: {name}</p>
       <p>Número: {count}</p>
 
@@ -19,8 +27,8 @@ export function App() {
     }}>Contar</button> */}
 
     <form action="">
-      <p>Palavra: {text}</p>
-      <p>Palavra: {text2}</p>
+      <p>Palavra1: {text}</p>
+      <p>Palavra2: {text2}</p>
       <input required type="text" onChange={(event) => setText(event.target.value)} />
       <button onClick={(event) => {
         event.preventDefault()
@@ -28,6 +36,11 @@ export function App() {
 
       }}>Enviar</button>
     </form>
+    <section>
+      <h2>{data.name}</h2>
+      {/* <img src={data.avatar_url} alt=""/>  */}
+      <p>{data.bio}</p>
+    </section>
     </div>
   )
 }
