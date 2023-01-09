6f4fbc42555b2145622aa89761dfae85c8ea8a3a
Ana Clara <Annaclarag2005@gmail.com>
2023-01-09 16:25:45 -0300

    add class 04 routes

---
+++ HTML/React/class-04-routes/src/pages/Gallery/index.jsx
@@ -0,0 +1,13 @@
+import reactLogo from '../../assets/react.svg'
+import './style.css'
+
+export function Gallery() {
+  return (
+    <div className="container">
+      <h1>Galeria</h1>
+      <img src={reactLogo} alt="" />
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit culpa neque in, accusantium repudiandae minus nam, qui, eligendi corrupti doloribus a! Perspiciatis tempore assumenda a aspernatur et ab corrupti eius.</p>
+     
+    </div>
+  )
+}
