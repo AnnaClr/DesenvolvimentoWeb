a1a664c038ae9db0709d3d2a25e0d7b88f901026
Ana Clara <Annaclarag2005@gmail.com>
2023-02-14 16:09:04 -0300

    class 02 NodeJS

---
+++ HTML/NodeJS/class-02-routes/src/routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require('express')
+const router = Router()
+
+router.get("/contact", (req, res) => {
+    res.send('Contato')
+})
+
+router.get("/", (req, res) => {
+    res.send('Class 02 - Routes Home')
+})
+
+router.get("/users", (req, res) => {
+    res.send("Users")
+})
+
+module.exports = router
