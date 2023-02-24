2a92785dd9ef7abc3a9cdf54fc31b20030d31228
Ana Clara <Annaclarag2005@gmail.com>
2023-02-24 16:53:54 -0300

    class 05

--- HTML/NodeJS/class-05-database/src/controllers/index.js
+++ HTML/NodeJS/class-05-database/src/controllers/index.js
@@ -1,17 +1,52 @@
+const prisma = require('../databases')
+
 module.exports = {
     async read(req, res) {
-        res.json({alert: "Ok"})
+        const { ID } = req.query;
+        if (ID) {
+            const listCourse = await prisma.cursoss.findUnique({where: {ID: Number(ID)}})
+            if (!listCourse) return res.json('User not found')
+            res.json(listCourse)
+        } else {
+            const listCourses = await prisma.cursoss.findMany()
+            res.json(listCourses)
+        }
     },
 
     async create(req, res) {
-        res.json({alert: "Ok"})
+       try {
+        const {Nome, Horas} = req.body
+        const createCourse = await prisma.cursoss.create({data: {Nome, Horas: Number(Horas)}})
+        res.json(createCourse)
+       } catch (error) {
+         return res.json(error.message)
+       }
     },
 
     async update(req, res) {
-        res.json({alert: "Ok"})
+        try {
+            const { ID } = req.params
+            const { Nome, Horas} = req.body
+            const updateCourse = await prisma.cursoss.update({
+                data: {Nome, Horas: Number(Horas)},
+                where: ({ID: Number(ID)})        
+            })
+            return res.json(updateCourse)
+        } catch {
+            return res.json(error.message)
+        }
     },
 
     async delete(req, res) {
-        res.json({alert: "Ok"})
+        try {
+            const { ID } = req.params
+    
+            const deleteCourse = await prisma.cursoss.delete({
+                where: ({ID: Number(ID)})        
+            })
+            return res.json(`Curso ${deleteCourse.Nome} deletada com sucesso!`)
+        } catch {
+            return res.json(error.message)
+        }
     }
 }
--- HTML/NodeJS/class-05-database/src/server.js
+++ HTML/NodeJS/class-05-database/src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, console.log('Server is running on Port 3000'))
+app.use(express.json())
 
 const routes = require('./routes')
 app.use(routes)
